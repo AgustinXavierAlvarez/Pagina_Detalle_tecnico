b0b8a52b38b3f3ba26b8b3341720c971cca55405
Agustin Xavier Alvarez <alvarezagustinx@gmail.com>
2022-07-22 12:43:54 -0300

    Actualizacion de la pagina

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express=require('express');
 const app=express();
 
-
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-
-
-app.listen (3050, () => { 
+app.listen (3040, () => { 
     console.log ('servidor corriendo')
 });
 
